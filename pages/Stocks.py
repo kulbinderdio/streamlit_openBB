1f488c2fcef855ed861fbb5d9b1611a3fb819785
kulbinderdio <kulbinderdio@gmail.com>
2022-12-19 17:13:42 +0000

    Stocks charting tidied up, added candlestick wiht volume bbar chart

--- pages/Stocks.py
+++ pages/Stocks.py
@@ -3,56 +3,138 @@ import streamlit as st
 import datetime
 import pandas as pd
 from st_aggrid import AgGrid
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
+# Candlestick code from 
+# https://medium.com/@dannygrovesn7/using-streamlit-and-plotly-to-create-interactive-candlestick-charts-a2a764ad0d8e
+
+ma1=10
+ma2=30
+days_to_plot=120
+
+def get_candlestick_plot(
+        df: pd.DataFrame,
+        ma1: int,
+        ma2: int,
+        ticker: str
+):
+    '''
+    Create the candlestick chart with two moving avgs + a plot of the volume
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The price dataframe
+    ma1 : int
+        The length of the first moving average (days)
+    ma2 : int
+        The length of the second moving average (days)
+    ticker : str
+        The ticker we are plotting (for the title).
+    '''
+    
+    fig = make_subplots(
+        rows = 2,
+        cols = 1,
+        shared_xaxes = True,
+        vertical_spacing = 0.1,
+        subplot_titles = (f'{ticker} Stock Price', 'Volume Chart'),
+        row_width = [0.3, 0.7]
+    )
+    
+    fig.add_trace(
+        go.Candlestick(
+            x = df['Date'],
+            open = df['Open'], 
+            high = df['High'],
+            low = df['Low'],
+            close = df['Close'],
+            name = 'Candlestick chart'
+        ),
+        row = 1,
+        col = 1,
+    )
+    
+    fig.add_trace(
+        go.Line(x = df['Date'], y = df[f'{ma1}_ma'], name = f'{ma1} SMA'),
+        row = 1,
+        col = 1,
+    )
+    
+    fig.add_trace(
+        go.Line(x = df['Date'], y = df[f'{ma2}_ma'], name = f'{ma2} SMA'),
+        row = 1,
+        col = 1,
+    )
+    
+    fig.add_trace(
+        go.Bar(x = df['Date'], y = df['Volume'], name = 'Volume'),
+        row = 2,
+        col = 1,
+    )
+    
+    fig['layout']['xaxis2']['title'] = 'Date'
+    fig['layout']['yaxis']['title'] = 'Price'
+    fig['layout']['yaxis2']['title'] = 'Volume'
+    
+    fig.update_xaxes(
+        rangebreaks = [{'bounds': ['sat', 'mon']}],
+        rangeslider_visible = False,
+    )
+    
+    return fig
+    
 
 st.set_page_config(layout="wide")
 
 
 homeTab, discoveryTab = st.tabs(["Home", "Discovery"])
 
+
+
 with homeTab:
-    text_input = st.text_input('Symbol')
+    ticker = st.text_input('Symbol')
 
-    st.metric('Symbol', text_input , delta=None, delta_color="inverse", help=None)
-    if text_input:
-        data = openbb.stocks.load(text_input)
-        
-        df_max_scaled = data.copy()
-    
-        # apply normalization techniques
-        for column in df_max_scaled.columns:
-            df_max_scaled[column] = df_max_scaled[column]  / df_max_scaled[column].abs().max()
-        
-        st.subheader('openbb.stocks.load')
-        st.line_chart(data=df_max_scaled,  x=None, y=None, width=0, height=0, use_container_width=True)
+    if ticker:
+        data = openbb.stocks.load(ticker)
 
-        col1, col2 = st.columns(2)
+        if len(data)>0:
+            
+            df = data.reset_index()
+            df.columns = [x.title() for x in df.columns]
 
-        with col1:
-            st.subheader('openbb.stocks.dd.supplier')
-            st.dataframe(openbb.stocks.dd.supplier(text_input))
+            df[f'{ma1}_ma'] = df['Close'].rolling(ma1).mean()
+            df[f'{ma2}_ma'] = df['Close'].rolling(ma2).mean()
+            df = df[-days_to_plot:]
 
-        with col2:
-            st.subheader('openbb.stocks.dd.customer')
-            st.dataframe(openbb.stocks.dd.customer(text_input))
+            # Display the plotly chart on the dashboard
+            st.plotly_chart(
+                get_candlestick_plot(df, ma1, ma2, ticker),
+                width=0, height=0,
+                use_container_width = True,
+            )
 
+            col1, col2 = st.columns(2)
 
-        # st.subheader('openbb.stocks.options.hist')
+            with col1:
+                st.subheader('openbb.stocks.dd.supplier')
+                st.dataframe(openbb.stocks.dd.supplier(ticker))
 
-        # d = st.date_input("Date")
+            with col2:
+                st.subheader('openbb.stocks.dd.customer')
+                st.dataframe(openbb.stocks.dd.customer(ticker))
+            
+            pd.set_option('display.max_columns', None)
+            st.subheader('openbb.stocks.options.info')
+            data = openbb.stocks.options.info(ticker)
+            new_data = pd.DataFrame.from_dict(data, orient='index')
 
-        # options_hist = openbb.stocks.options.hist(f"{text_input}", d, 150, call=True, source="ChartExchange")
-        # st.dataframe(options_hist)
-        
-        pd.set_option('display.max_columns', None)
-        st.subheader('openbb.stocks.options.info')
-        data = openbb.stocks.options.info(text_input)
-        new_data = pd.DataFrame.from_dict(data, orient='index')
+            new_data = new_data.reset_index(level=0)
+            new_data.columns=['Heading','Value']
 
-        new_data = new_data.reset_index(level=0)
-        new_data.columns=['Heading','Value']
-
-        AgGrid(new_data, height=300)
+            AgGrid(new_data, height=300)
+        else:
+            st.write('No Data Found for '+ticker)
 
 
 with discoveryTab:
